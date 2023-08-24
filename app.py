7722bb56fa20bc388b647de3f1635a995814afd4
victoriacline <v.cline1124@gmail.com>
2023-08-24 17:01:48 -0600

    501

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
+#variables
 price = 10
 price = 20
+name = "Victoria"
+is_published = False
 print(price)
+#variable practice
+name = "John Smith"
+age = 20
+is_new = True
+'''
+#user input
+name = input("What is your name? ")
+print("Hi, " + name)
+'''
+#user input practice
+your_name = input("what is your name? ")
+color = input("what is your favorite color? ")
+print(your_name + " likes " +color)
+
+
