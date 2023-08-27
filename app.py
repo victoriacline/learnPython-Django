4eec11564a083a78ae5f282afdbeae61a3a2df60
victoriacline <v.cline1124@gmail.com>
2023-08-27 12:31:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,24 @@ print(price)
 name = "John Smith"
 age = 20
 is_new = True
+
 '''
 #user input
 name = input("What is your name? ")
 print("Hi, " + name)
-'''
+
 #user input practice
 your_name = input("what is your name? ")
 color = input("what is your favorite color? ")
 print(your_name + " likes " +color)
+'''
+#type conversion
+birth_year = input("Birth year: ")
+age = 2023 - int(birth_year)
+print(age)
+#type conversion practice
+pounds = input("Weight in pounds: ")
+kilos = int(pounds) * 0.45
+print(kilos)
 
-
+ 
